e9fc680d078a6ef148cecd37787315d3f09c3a32
back hae ran <idg9833@naver.com>
2022-09-15 14:30:04 +0900

    springbootPractice init

---
+++ springbootPractice/hello/hello/src/main/java/com/example/hello/Controller/GetController.java
@@ -0,0 +1,34 @@
+package com.example.hello.Controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/get-api")
+public class GetController {
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public  String getHello(){
+        return "Hello world";
+    }
+
+
+    @GetMapping(value = "/name")
+    public  String getName(){
+        return "haeran";
+    }
+
+    @GetMapping(value = "/variable1/{variable}")
+    public  String getVariable1(@PathVariable String variable){
+        return variable;
+    }
+
+    @GetMapping(value = "/variable2/{variable}")
+    public  String getVariable2(@PathVariable("variable") String var){
+        return var;
+    }
+
+    @GetMapping(value = "/request1")
+    public  String getRequestParam1(@RequestParam String name, @RequestParam String email, @RequestParam String organization){
+        return name+""+email+""+organization;
+    }
+
+}
